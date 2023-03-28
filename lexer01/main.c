a63f77991913eb1a84cd9c29957779b2db157174
RAFAEL RIBAS ALBUQUERQUE <rafael.ralbuquerque@senacsp.edu.br>
2023-03-27 21:26:07 -0300

    versao 1

---
+++ lexer01/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum token {
+	Mais,
+	Menos,
+	Vezes,
+	Divisão
+}tokens;
+
+typedef struct {
+	char token;
+	char valor;
+}caracteres;
+
+void lexer(FILE *string){
+
+	char ch;
+	int count = 0;
+	while((ch=fgetc(string))  != EOF ){
+      count++;
+	}
+
+	char *texto = (char *) malloc(count);
+	
+	rewind(string);
+
+	for (int i = 0; i < count; i++){
+		texto[i] = fgetc(string);
+	}
+
+	
+	for (int i = 0; i < count; i++){
+		switch(texto[i]) {
+			case '+':
+				printf(" Mais \n"); 
+				break;
+			case '-':
+				printf(" Menos \n");
+				break;
+			case '*':
+				printf(" Vezes \n");
+				break;
+			case '/':
+				printf(" Divisão \n");	
+				break;
+			case ' ':
+				break;
+			default:
+				printf(" Indeterminado ' %c '\n", texto[i]);
+				break;
+		}
+	}
+}
+int main(int numargs, char *arg[]) {
+
+	char nome_arquivo[100];
+	FILE *arquivo = fopen(arg[1], "r");
+
+	if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo %s\n", nome_arquivo);
+        return 1;
+  }
+
+  // // Lê o conteúdo do arquivo
+  //   char caractere;
+  //   while ((caractere = fgetc(arquivo)) != EOF) {
+			
+		// }
+
+		lexer(arquivo);
+
+  // Fecha o arquivo
+  fclose(arquivo);
+	
+  return 0;
+}
